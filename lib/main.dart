5443504e22529fec6d4415762c796f5609adbd64
Xgor <real.xgor@hotmail.com>
2024-11-26 23:19:34 +0100

    Added numbers and colors to clock

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,9 @@ class MyApp extends StatelessWidget {
       title: 'Decimal Clock',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
         useMaterial3: true,
+      scaffoldBackgroundColor: Color.fromARGB(255, 169, 175, 179),
       ),
       home: const MyHomePage(title: 'Decimal Clock'),
     );
@@ -98,14 +100,16 @@ class _MyHomePageState extends State<MyHomePage> {
               '$dec_hour:$dec_minute:$dec_second',
               style: TextStyle(fontFamily: 'Aileron Black', fontSize: 50),
             ),
+            
           ],
         ),
       ),
+      /*
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), */
     );
   }
 
@@ -150,8 +154,8 @@ class _DecClockWidget extends State<DecClockWidget> {
     return CustomPaint(
       painter: DecClock(timeOfDay),
       child: Container(
-        width: 500,
-        height: 500,
+        width: 400,
+        height: 400,
       ),
     );
   }
@@ -176,16 +180,18 @@ class DecClock extends CustomPainter {
   final Duration timeOfDay;
 
   DecClock(this.timeOfDay);
+  
+
 
   double percentToRad(double val)
   {
     return math.pi*2/(100/val);
   }
 
-  void drawHourLine(Canvas canvas,Offset centerOffset,double radian,Paint paint,int inner)
+  void drawHourLine(Canvas canvas,Offset centerOffset,double radian,Paint paint,double inner,double outer)
   {
   //  final inner = 180;
-    final outer = 200;
+  //  final outer = 200;
 
     var innerOffset = Offset(centerOffset.dx+math.sin(radian)*inner, centerOffset.dy-math.cos(radian)*inner);
     var outerOffset = Offset(centerOffset.dx+math.sin(radian)*outer, centerOffset.dy-math.cos(radian)*outer);
@@ -195,10 +201,11 @@ class DecClock extends CustomPainter {
   }
   @override
   void paint(Canvas canvas, Size size) {
-    final milliSize = 200;
-    final secSize = 160;
-    final minSize = 130;
-    final hourSize = 100;
+    final halfWidth = size.width*0.5;
+    final milliSize = halfWidth*.8;
+    final secSize = halfWidth*.7;
+    final minSize = halfWidth*.6;
+    final hourSize = halfWidth*.5;
 
     var center = size / 2;
     var centerOffset = Offset(center.width, center.height);
@@ -207,46 +214,119 @@ class DecClock extends CustomPainter {
     var rad_milliSeconds = percentToRad(((milliseconds).floor()%100));
     var seconds = percentToRad((timeOfDay.inMilliseconds/86400)%100);
     var minute = percentToRad((timeOfDay.inSeconds/8640)%100);
-    var hour = percentToRad((timeOfDay.inHours/ 24*100));
+    var hour = percentToRad((timeOfDay.inMinutes/ 1440*100));
    // percentToRad((timeOfDay.inHours/ 24*10));
 
-    var paint = Paint()..color = Color.fromARGB(255, 85, 56, 53);
+
+    var paint = Paint()..color = Color.fromARGB(255, 85, 56, 53)..strokeWidth = 2;
     var minutesPaint = Paint()..color = Color.fromARGB(255, 37, 22, 21) ..strokeWidth = 4;
- //   var hourPaint = Paint()..color = Color.fromARGB(255, 82, 22, 20) ..strokeWidth = 6;
     var dayPaint = Paint()..color = Color.fromARGB(255, 2, 63, 7) ..strokeWidth = 10;
 
     var secondsPointerOffset = Offset(center.width+math.sin(rad_milliSeconds)*milliSize, center.height-math.cos(rad_milliSeconds)*milliSize);
     var minutesPointerOffset = Offset(center.width+math.sin(seconds)*secSize, center.height-math.cos(seconds)*secSize);
- //   var hoursPointerOffset = Offset(center.width+math.sin(minute)*minSize, center.height-math.cos(minute)*minSize);
     var dayPointerOffset = Offset(center.width+math.sin(hour)*hourSize, center.height-math.cos(hour)*hourSize);
+
+    var cornerPaint = Paint()..color = Color.fromARGB(255, 209, 139, 33) ..strokeWidth = 1;
+    var innerPaint = Paint()..color = Color.fromARGB(255, 169, 174, 175) ..strokeWidth = 1;
+    var insidePaint = Paint()..color = Color.fromARGB(255, 255, 255, 255) ..strokeWidth = 1;
+    canvas.drawCircle(centerOffset, halfWidth*1.08, cornerPaint);
+    canvas.drawCircle(centerOffset, halfWidth*1.06, insidePaint);
+   // canvas.drawCircle(centerOffset, halfWidth*0.8, innerPaint);
 
     canvas.drawLine(centerOffset,secondsPointerOffset,paint);
     canvas.drawLine(centerOffset,minutesPointerOffset,minutesPaint);
-//    canvas.drawLine(centerOffset,hoursPointerOffset,hourPaint);
     canvas.drawLine(centerOffset,dayPointerOffset,dayPaint);
-    
+
+
+
+
 
     var mayorLine = Paint()..color = Color.fromARGB(255, 37, 22, 21) ..strokeWidth = 4;
     var minorLine = Paint()..color = Color.fromARGB(255, 180, 180, 180) ..strokeWidth = 1;
     for (var i = 1; i <= 10; i++) {
-      for (var j = 1; j <= 10; j++) {
-        drawHourLine(canvas,centerOffset,math.pi*((.2*i)+(j*.02)),minorLine,190);
+      for (var j = 1; j < 10; j++) {
+
+        drawHourLine(canvas,centerOffset,math.pi*((.2*i)+(j*.02)),minorLine,halfWidth*.9,halfWidth*.95);
+
       }
-      drawHourLine(canvas,centerOffset,math.pi*.2*i,mayorLine,180);
+     // final radian = math.pi*.2*i; 
+      drawHourLine(canvas,centerOffset,math.pi*.2*i,mayorLine,halfWidth*.85,halfWidth*.95);
+      //textPainter.textScaler
+    //  final textOffset = Offset(centerOffset.dx+math.sin(radian)*halfWidth*1.1, centerOffset.dy-math.cos(radian)*halfWidth*1.1);
+ //     textPainter1.paint(canvas, textOffset);
     }
+
+
     canvas.drawCircle(centerOffset, 10, minutesPaint);
-    /*
-    canvas.drawArc(
-      Rect.fromCenter(
-        center: Offset(center.width, center.height),
-        width: 50,
-        height: 50,
-      ),
-      0.4,
-      2 * pi - 0.8,
-      true,
-      paint,
-    );*/
+    centerOffset = centerOffset-Offset(6,16);
+
+    const textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 25,
+   //   fontFamily: 'Aileron Black',
+    );
+    final textPainter1 = TextPainter(
+      text: const TextSpan(text: '1',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter1.layout();
+    final textPainter2 = TextPainter(
+      text: const TextSpan(text: '2',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter2.layout();
+    final textPainter3 = TextPainter(
+      text: const TextSpan(text: '3',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter3.layout();
+      final textPainter4 = TextPainter(
+      text: const TextSpan(text: '4',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter4.layout();
+      final textPainter5 = TextPainter(
+      text: const TextSpan(text: '5',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter5.layout();
+    final textPainter6 = TextPainter(
+      text: const TextSpan(text: '6',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter6.layout();
+    final textPainter7 = TextPainter(
+      text: const TextSpan(text: '7',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter7.layout();
+    final textPainter8 = TextPainter(
+      text: const TextSpan(text: '8',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter8.layout();
+      final textPainter9 = TextPainter(
+      text: const TextSpan(text: '9',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter9.layout();
+      final textPainter10 = TextPainter(
+      text: const TextSpan(text: '10',style: textStyle),
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    );  textPainter10.layout();
+
+    textPainter1.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*.2)*halfWidth, centerOffset.dy-math.cos(math.pi*.2)*halfWidth));
+    textPainter2.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*.4)*halfWidth, centerOffset.dy-math.cos(math.pi*.4)*halfWidth));
+    textPainter3.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*.6)*halfWidth, centerOffset.dy-math.cos(math.pi*.6)*halfWidth));
+    textPainter4.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*.8)*halfWidth, centerOffset.dy-math.cos(math.pi*.8)*halfWidth));
+    textPainter5.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*1)*halfWidth, centerOffset.dy-math.cos(math.pi*1)*halfWidth));
+    textPainter6.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*1.2)*halfWidth, centerOffset.dy-math.cos(math.pi*1.2)*halfWidth));
+    textPainter7.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*1.4)*halfWidth, centerOffset.dy-math.cos(math.pi*1.4)*halfWidth));
+    textPainter8.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*1.6)*halfWidth, centerOffset.dy-math.cos(math.pi*1.6)*halfWidth));
+    textPainter9.paint(canvas, Offset(centerOffset.dx+math.sin(math.pi*1.8)*halfWidth, centerOffset.dy-math.cos(math.pi*1.8)*halfWidth));
+    textPainter10.paint(canvas, Offset(centerOffset.dx+math.sin(0)*halfWidth-8, centerOffset.dy-math.cos(0)*halfWidth));
+    
   }
 
   
